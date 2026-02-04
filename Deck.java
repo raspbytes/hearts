a5a4c5409ced4ae0ffdc200c499f674bf000c79d
raspbyte_ <sshafi8511@gmail.com>
2026-02-04 16:20:10 -0600

    added deck init code (0204)

---
+++ Deck.java
@@ -0,0 +1,11 @@
+public class Deck{
+  private ArrayList<Deck>();
+
+  public static final String[] NAMES = {"2","3","4","5","6","7","8","9","T","J","Q","K","A"};
+  public static final String[] SUITS = {"clubs","spades","hearts","diamonds"};
+  public static final int[] RANKS = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+  public static Deck(){
+    //add deck code later
+  }
+}

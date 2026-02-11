ced9cc9f211af29123a84baee868aa0e02c9556e
sshafi <sshafi8511@gmail.com>
2026-02-10 21:06:41 -0600

    AAAAAAAAAAAAAHHHHHH

--- Deck.java
+++ Deck.java
@@ -5,7 +5,7 @@ public class Deck{
 
   public static final String[] NAMES = {"2","3","4","5","6","7","8","9","T","J","Q","K","A"};
   public static final String[] SUITS = {"clubs","spades","hearts","diamonds"};
-  public static final int[] RANKS = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+  public static final int[] RANKS = {2,3,4,5,6,7,8,9,10,11,12,13,1};
   
   public  Deck(){
     initializeDeck();

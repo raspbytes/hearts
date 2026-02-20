fc9e864bfacacbb1a3c285ef1d985f1a05b1f62e
sshafi <sshafi8511@gmail.com>
2026-02-19 19:17:52 -0600

    reuploaded the same code because i'm dumb

--- Deck.java
+++ Deck.java
@@ -1,3 +1,4 @@
+//Name: Sarah Shafi
 import java.util.ArrayList;
 
 public class Deck{
@@ -5,7 +6,7 @@ public class Deck{
 
   public static final String[] NAMES = {"2","3","4","5","6","7","8","9","T","J","Q","K","A"};
   public static final String[] SUITS = {"clubs","spades","hearts","diamonds"};
-  public static final int[] RANKS = {2,3,4,5,6,7,8,9,10,11,12,13,1};
+  public static final int[] RANKS = {2,3,4,5,6,7,8,9,10,11,12,13,14};
   
   public  Deck(){
     initializeDeck();
